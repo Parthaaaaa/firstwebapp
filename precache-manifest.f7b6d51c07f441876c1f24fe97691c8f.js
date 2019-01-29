177779cd6f5355d34cfbac5baef0b69e2a1ccb3a
Partha <partho01a@gmail.com>
2019-01-29 22:14:53 +0600

    Updates

---
+++ precache-manifest.f7b6d51c07f441876c1f24fe97691c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bf30e52743f2bd12ad",
+    "url": "Parthaaaaa.github.io/firstwebapp/static/js/runtime~main.81bf30e5.js"
+  },
+  {
+    "revision": "4c643761b1e60ed05f6e",
+    "url": "Parthaaaaa.github.io/firstwebapp/static/js/main.4c643761.chunk.js"
+  },
+  {
+    "revision": "fd0e8539050bc422773e",
+    "url": "Parthaaaaa.github.io/firstwebapp/static/js/1.fd0e8539.chunk.js"
+  },
+  {
+    "revision": "4c643761b1e60ed05f6e",
+    "url": "Parthaaaaa.github.io/firstwebapp/static/css/main.01c5ff51.chunk.css"
+  },
+  {
+    "revision": "fd0e8539050bc422773e",
+    "url": "Parthaaaaa.github.io/firstwebapp/static/css/1.58c735f3.chunk.css"
+  },
+  {
+    "revision": "5e03823818f7a9b252b901a21820768c",
+    "url": "Parthaaaaa.github.io/firstwebapp/index.html"
+  }
+];
